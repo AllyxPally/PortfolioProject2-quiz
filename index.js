69b29b84e9a4d599f2c85c7bca797ac960fc20d0
Alicia G <ladyeleniel@hotmail.com>
2024-05-29 14:37:35 +0000

    add index.js and quiz questions

---
+++ index.js
@@ -0,0 +1,22 @@
+const quizData = [
+    {
+   question: 'What is the name of the princess in Super Mario?',
+   options: ['Rose', 'Zelda', 'Peach', 'Megan'],
+   right: '2',
+},
+{
+    question: 'Who is the author of Harry Potter?',
+    options: ['Stephanie Meyer', 'J.K. Rowling', 'Paula Hawkins', 'Suzanne Collins'],
+    right: '1',  
+},
+{
+    question: 'Who won the FIFA Women´s World Cup 2023?',
+    options: ['England', 'France', 'Australia', 'Spain'],
+    right:  '3', 
+},
+{
+    question: 'Where were Frida Kahlo born?',
+    options: ['Mexico', 'Argentina', 'Peru', 'Bolivia'],
+    right: '0',
+},
+];
