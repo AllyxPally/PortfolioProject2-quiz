766e994c048a96519a4d7b2822dd36f75fd29f16
Alicia G <ladyeleniel@hotmail.com>
2024-06-09 15:57:47 +0000

    FIX: Fix bug causing quiz not to work

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
-// Quiz 
+// Quiz
 const quizData = [
     {
-   question: 'What is the name of the princess who wears a pink dress in Super Mario?',
-   options: ['Rose', 'Zelda', 'Peach', 'Megan'],
-   right: '2',
-},
-{
-    question: 'Who is the author of Harry Potter?',
-    options: ['Stephanie Meyer', 'J.K. Rowling', 'Paula Hawkins', 'Suzanne Collins'],
-    right: '1',  
-},
-{
-    question: 'Who won the FIFA Women´s World Cup 2023?',
-    options: ['England', 'France', 'Australia', 'Spain'],
-    right:  '3', 
-},
-{
-    question: 'Where were Frida Kahlo born?',
-    options: ['Mexico', 'Argentina', 'Peru', 'Bolivia'],
-    right: '0',
-},
+        question: 'What is the name of the princess who wears a pink dress in Super Mario?',
+        options: ['Rose', 'Zelda', 'Peach', 'Megan'],
+        right: 2
+    },
+    {
+        question: 'Who is the author of Harry Potter?',
+        options: ['Stephanie Meyer', 'J.K. Rowling', 'Paula Hawkins', 'Suzanne Collins'],
+        right: 1
+    },
+    {
+        question: 'Who won the FIFA Women´s World Cup 2023?',
+        options: ['England', 'France', 'Australia', 'Spain'],
+        right: 3
+    },
+    {
+        question: 'Where were Frida Kahlo born?',
+        options: ['Mexico', 'Argentina', 'Peru', 'Bolivia'],
+        right: 0
+    },
 ];
 
-let validQuestion = 0; 
-let rightAnswers = 0; 
+let avaliableQuestions = 0;
+let score = 0;
 
 // Function to display the question to the user
 
 function displayQuestion() {
-    const quizText = document.getElementsByClassName("quiz_text");
-    quizText.quizContent = quizData[validQuestion].quiz;
+    const quizText = document.getElementById("quiz_text");
+    quizText.textContent = quizData[avaliableQuestions].question;
 
-    const options = document.getElementsByClassName("option");
+    const options = document.querySelectorAll(".option");
     options.forEach((option, index) => {
-    option.quizContent = quizData[validQuestion].options;
+        option.textContent = quizData[avaliableQuestions].options[index];
     });
-    const feedback = document.getElementsByClassName("feedback");
-    feedback.quizContent = "";
+    
+    const feedback = document.getElementById("feedback");
+    feedback.textContent = "";
 }
 // Function to check if the answer is right 
 
 function checkAnswer(chosenAnswer) {
-const feedback = document.getElementsByClassName("feedback");
-if (chosenAnswer === quizData[validQuestion].right) {
-    feedback.quizContent = "Right!";
-rightAnswers++;
-} else {
-    feedback.quizContent = "Wrong!";
+    const feedback = document.getElementById("feedback");
+    if (chosenAnswer === quizData[avaliableQuestions].right) {
+        feedback.textContent = "Right!";
+        score++;
+    } else {
+        feedback.textContent = "Wrong!";
+    }
+
+    setTimeout (() => {
+    avaliableQuestions++;
+    
+    if (avaliableQuestions < quizData.length) {
+        displayQuestion();
+    } else {
+        const quizContainer = document.querySelector(".quiz_container");
+        quizContainer.innerHTML = `<p>You're result is ${score} out of ${quizData.length}</p>`;
+    }
+}, 1000);
 }
-validQuestion++; 
-if(validQuestion < quizData.length) {
-displayQuestion();
-} else {
-    const quizContainer = document.getElementsByClassName("quiz_container");
-    quizContainer.innerHTML = `<p>You're result is ${rightAnswers} out of ${quizData.length}</p>`;
-}
-};
 
 displayQuestion();
